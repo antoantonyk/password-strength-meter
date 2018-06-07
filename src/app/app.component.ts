5d0d9c8fd6519c049d613c284fe038b7bdf7aa0b
antoantonyk <antoant02@ent.bhicorp.com>
2018-06-07 11:45:54 +0530

    fix: bootstrap form validation fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,6 @@ import * as zxcvbn from 'zxcvbn';
 })
 export class AppComponent {
   userForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.userForm = this.fb.group({
-      fullname: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
-
   get fullname() {
     return this.userForm.get('fullname');
   }
@@ -31,8 +22,26 @@ export class AppComponent {
     return this.userForm.get('password');
   }
 
-  onSubmit() {
-    console.log('this.userForm.value', this.userForm.value);
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      fullname: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('====================================');
+      console.log('userForm', this.userForm);
+      console.log('====================================');
+    } else {
+      console.log('====================================');
+      console.log('Invalid Form');
+      console.log('====================================');
+      Object.keys(this.userForm.controls).forEach(key => {
+        this.userForm.get(key).markAsDirty();
+      });
+    }
+  }
 }
