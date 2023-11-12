9d48ba4fc5514c6b2d8b28db85f0efecd00437ad
antoantonyk <antoantony8@gmail.com>
2023-11-12 19:33:42 +0100

    🐛 FIX: unit test cases

--- projects/password-strength-meter-showcase/src/app/module/custom-module/custom-component/custom-component.component.spec.ts
+++ projects/password-strength-meter-showcase/src/app/module/custom-module/custom-component/custom-component.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomComponentComponent } from './custom-component.component';
+import { PasswordStrengthMeterComponent } from 'angular-password-strength-meter';
+import { FormsModule } from '@angular/forms';
+import { provideZxvbnServiceForPSM } from 'angular-password-strength-meter/zxcvbn';
 
 describe('CustomComponentComponent', () => {
   let component: CustomComponentComponent;
@@ -8,10 +11,11 @@ describe('CustomComponentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CustomComponentComponent]
-    })
-    .compileComponents();
-    
+      imports: [PasswordStrengthMeterComponent, FormsModule],
+      declarations: [CustomComponentComponent],
+      providers: [provideZxvbnServiceForPSM()],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CustomComponentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
